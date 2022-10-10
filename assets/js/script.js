89bddc3c429de63d5b9e75fb38583118b81366b5
Louise Stanley <somekindofdesign@gmail.com>
2022-10-10 16:26:52 +0000

    Add initial js for game play

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+/**
+ * Code Institute Rock Paper Scissors Spock Example Game
+ */
+
+const buttons = documents.getElementByClassName("control");
+const playerScore = documents.getElementById("player-score");
+const computerScore = documents.getElementById("computer-score");
+const playerImage = documents.getElementById("player-image");
+const computerImage = documents.getElementById("computer-image");
+const messages = documents.getElementById("messages");
+const choices = ["rock", "paper", "scissors"];
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = getAttribute("data-choice");
+        playerGame(playerChoice);
+    });
+}
+
+function playGame(playerChoice) {
+
+    playerImage.src = `assets/images/{$choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    computerImage.src = `assets/images/{$choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+
+}
