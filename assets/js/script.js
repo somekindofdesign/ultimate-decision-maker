74556f35fae64ee08b65205379059e67b450b258
Louise Stanley <somekindofdesign@gmail.com>
2022-10-14 09:05:15 +0000

    Add new const for js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const computerChoiceText = document.getElementById('computerChoice')
+const playerChoiceText = document.getElementById('playerChoice')
+const resultText = document.getElementById('result')
+const allChoices = document.querySelectorAll('button')
+let playerChoice
+let computerChoice
