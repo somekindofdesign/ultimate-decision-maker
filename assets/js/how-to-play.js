ca83d2ec1423af7d7f16c5d8e3618315a8972ee5
Louise Stanley <somekindofdesign@gmail.com>
2022-10-18 15:31:31 +0000

    Add icons to homepage

--- assets/js/how-to-play.js
+++ assets/js/how-to-play.js
@@ -1,29 +1,30 @@
-function CompareChoices(oneChoice, twoChoice) {
-    var oneChoice = document.getElementById('oneChoice');
-    var twoChoice = document.getElementById('twoChoice');
-    const allChoices = document.querySelectorAll('button')
-  
-  if (oneChoice.selectedIndex === twoChoice.selectedIndex) {
-    if (twoChoice === oneChoice) {
-        result = "It's a tie!"
-      }
-      if (twoChoice === 'rock' && oneChoice === "paper") {
-        result = 'Paper wins'
-      }
-      if (twoChoice === 'rock' && oneChoice === "scissors") {
-        result = 'Rock wins'
-      }
-      if (twoChoice === 'paper' && oneChoice === "rock") {
-        result = 'Paper wins'
-      }
-      if (twoChoice === 'paper' && oneChoice === "scissors") {
-        result = 'Scissors wins'
-      }
-      if (twoChoice === 'scissors' && oneChoice === "rock") {
-        result = 'Rock wins'
-      }
-      if (twoChoice === 'scissors' && oneChoice === "paper") {
-        result = 'Scissors wins'
-      }
-      resultText.innerHTML = result
-  }
+function compare(oneChoice, twoChoice) {
+  var oneChoice = document.getElementById('oneChoice');
+  var twoChoice = document.getElementById('twoChoice');
+  const compareChoices = document.querySelectorAll('button')
+
+if (oneChoice.selectedIndex === twoChoice.selectedIndex) {
+  if (twoChoice === oneChoice) {
+      result = "It's a tie!"
+    }
+    if (twoChoice === 'rock' && oneChoice === "paper") {
+      result = 'Paper wins'
+    }
+    if (twoChoice === 'rock' && oneChoice === "scissors") {
+      result = 'Rock wins'
+    }
+    if (twoChoice === 'paper' && oneChoice === "rock") {
+      result = 'Paper wins'
+    }
+    if (twoChoice === 'paper' && oneChoice === "scissors") {
+      result = 'Scissors wins'
+    }
+    if (twoChoice === 'scissors' && oneChoice === "rock") {
+      result = 'Rock wins'
+    }
+    if (twoChoice === 'scissors' && oneChoice === "paper") {
+      result = 'Scissors wins'
+    }
+    resultText.innerHTML = result
+}
+}
