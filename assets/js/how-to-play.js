db4cf77c2d84b79bc36bd238e12a87c01b74f803
Louise Stanley <somekindofdesign@gmail.com>
2022-10-18 11:04:45 +0000

    Add initial html and js for compare tool on how to page

---
+++ assets/js/how-to-play.js
@@ -0,0 +1,28 @@
+function CompareChoices(oneChoice, twoChoice) {  
+    var oneChoice = document.getElementById('oneChoice');  
+    var twoChoice = document.getElementById('twoChoice');  
+  
+  if (oneChoice.selectedIndex === twoChoice.selectedIndex) {
+    if (twoChoice === oneChoice) {
+        result = "It's a tie!"
+      }
+      if (twoChoice === 'rock' && oneChoice === "paper") {
+        result = 'Paper wins'
+      }
+      if (twoChoice === 'rock' && oneChoice === "scissors") {
+        result = 'Rock wins'
+      }
+      if (twoChoice === 'paper' && oneChoice === "rock") {
+        result = 'Paper wins'
+      }
+      if (twoChoice === 'paper' && oneChoice === "scissors") {
+        result = 'Scissors wins'
+      }
+      if (twoChoice === 'scissors' && oneChoice === "rock") {
+        result = 'Rock wins'
+      }
+      if (twoChoice === 'scissors' && oneChoice === "paper") {
+        result = 'Scissors wins'
+      }
+      resultText.innerHTML = result
+  }
