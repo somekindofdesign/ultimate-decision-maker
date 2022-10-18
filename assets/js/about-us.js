1b6e1d259913bb9f712d1ebdafe63c6a5ca005fa
Louise Stanley <somekindofdesign@gmail.com>
2022-10-18 14:32:45 +0000

    Add form submitted message on about page

---
+++ assets/js/about-us.js
@@ -0,0 +1,7 @@
+const submitButton = document.getElementById('submit');
+const submittedText = document.getElementById('submitted');
+
+submitButton.addEventListener('click', function(){
+  const myInsertText = "Submitted! We can't wait to work with you :)";
+submittedText.innerHTML = myInsertText;
+});
