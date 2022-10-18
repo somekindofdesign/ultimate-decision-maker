58cb2fc071d1012b9dee18cdcffaa6064caf515b
Louise Stanley <somekindofdesign@gmail.com>
2022-10-18 16:03:10 +0000

    Attempt fix for submit button

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -1,8 +1,7 @@
 const submitButton = document.getElementById('submit');
 const submittedText = document.getElementById('submitted');
-const insertText =
 
-submitButton.addEventListener('click', function() {
-  function myInsertText() = "Submitted! We can't wait to work with you :)";
+submitButton.addEventListener('click', function(){
+    const myInsertText = "Submitted! We can't wait to work with you :)";
   submittedText.innerHTML = myInsertText;
-});
+  });
