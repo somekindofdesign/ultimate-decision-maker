4cb9d318d66f9e7664e8081e2d089623a998ce83
Louise Stanley <somekindofdesign@gmail.com>
2022-10-25 09:13:13 +0000

    Add 90s style cursor html & js to about us page

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -1,3 +1,7 @@
+// 90's style cursor courtesy of https://github.com/tholman/cursor-effects
+new cursoreffects.ghostCursor();
+
+// submit button
 const submitButton = document.getElementById('submit');
 const submittedText = document.getElementById('submitted');
 
