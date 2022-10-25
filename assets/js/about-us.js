55300a17e0af0d527f85d75cf34be832bf8f31ed
Louise Stanley <somekindofdesign@gmail.com>
2022-10-25 12:10:21 +0000

    Remove broken js for form submit

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -1,11 +1,2 @@
 // 90's style cursor courtesy of https://github.com/tholman/cursor-effects
 new cursoreffects.ghostCursor();
-
-// submit button
-const submitButton = document.getElementById('submit');
-const submittedText = document.getElementById('submitted');
-
-submitButton.addEventListener('click', function(){
-    const myInsertText = "Submitted! We can't wait to work with you :)";
-  submittedText.innerHTML = myInsertText;
-  });
