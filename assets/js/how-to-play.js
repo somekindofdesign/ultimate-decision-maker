1317c3ce3fb8229a05d30f78c4448d5d08f44aaa
Louise Stanley <somekindofdesign@gmail.com>
2022-10-26 10:29:42 +0000

    Try again for how to function using game js

--- assets/js/how-to-play.js
+++ assets/js/how-to-play.js
@@ -1,41 +1,93 @@
-//compare function
-function compare(oneChoice, twoChoice) {
-  var oneChoice = document.getElementById('oneChoice');
-  var twoChoice = document.getElementById('twoChoice');
-  var resultText = document.getElementById('result');
-  const compareChoices = document.querySelectorAll('button');
+const resultText = document.getElementById('result');
+const allChoices = document.querySelectorAll('button');
+let oneChoice;
+let twoChoice;
+let result;
 
-  compareChoices.forEach(aChoice => aChoice.addEventListener('click', (e) => {
+allChoices.forEach(aChoice => aChoice.addEventListener('click', (e) => {
     oneChoice = e.target.id;
     twoChoice = e.target.id;
     getResult();
   }));
 
-//assess choices and determine result
-if (oneChoice.selectedIndex === twoChoice.selectedIndex) {
-  if (twoChoice === oneChoice) {
+function getResult() {
+
+    //tie
+    if (oneChoice === twoChoice) {
       result = "It's a tie!";
     }
-    if (twoChoice === 'rock' && oneChoice === "paper") {
-      result = 'Paper wins';
+    
+    ///rock
+    if (oneChoice === 'rock' && twoChoice === "paper") {
+      result = 'Rock covers paper - rock wins';
     }
-    if (twoChoice === 'rock' && oneChoice === "scissors") {
-      result = 'Rock wins';
+    if (oneChoice === 'rock' && twoChoice === "scissors") {
+      result = 'Rock crushes scissors - rock wins';
     }
-    if (twoChoice === 'paper' && oneChoice === "rock") {
-      result = 'Paper wins';
+    if (oneChoice === 'rock' && twoChoice === "lizard") {
+      result = 'Rock crushes lizard - rock wins';
     }
-    if (twoChoice === 'paper' && oneChoice === "scissors") {
-      result = 'Scissors wins';
-    }
-    if (twoChoice === 'scissors' && oneChoice === "rock") {
-      result = 'Rock wins';
-    }
-    if (twoChoice === 'scissors' && oneChoice === "paper") {
-      result = 'Scissors wins';
+    if (oneChoice === 'rock' && twoChoice === "spock") {
+      result = 'Spock vaporizes rock - Spock wins';
     }
 
-    //insert result to html
+    ///paper
+    if (oneChoice === 'paper' && twoChoice === "rock") {
+      result = 'Paper covers rock - paper wins';
+    }
+    if (oneChoice === 'paper' && twoChoice === "scissors") {
+      result = 'Scissors cuts paper - paper wins';
+    }
+    if (oneChoice === 'paper' && twoChoice === "lizard") {
+      result = 'Lizard eats paper - Lizard wins';
+    }
+    if (oneChoice === 'paper' && twoChoice === "spock") {
+      result = 'Paper disproves Spock - paper wins';
+    }
+
+    ///scissors
+    if (oneChoice === 'scissors' && twoChoice === "rock") {
+      result = 'Rock crushes scissors - rock wins';
+    }
+    if (oneChoice === 'scissors' && twoChoice === "paper") {
+      result = 'Scissors cuts paper - scissors wins';
+    }
+    if (oneChoice === 'scissors' && twoChoice === "lizard") {
+      result = 'Scissors decapitates lizard - scissors wins';
+    }
+    if (oneChoice === 'scissors' && twoChoice === "spock") {
+      result = 'Spock smashes scissors - Spock wins';
+    }
+
+    ///lizard
+    if (oneChoice === 'lizard' && twoChoice === "rock") {
+      result = 'Rock crushes lizard - rock wins';
+    }
+    if (oneChoice === 'lizard' && twoChoice === "paper") {
+      result = 'Lizard eats paper - lizard wins';
+    }
+    if (oneChoice === 'lizard' && twoChoice === "scissors") {
+      result = 'Scissors decapitates lizard - scissors wins';
+    }
+    if (oneChoice === 'lizard' && twoChoice === "spock") {
+      result = 'Lizard poisons Spock - lizard wins';
+    }
+
+    ///spock
+    if (oneChoice === 'spock' && twoChoice === "rock") {
+      result = 'Spock vaporizes rock - Spock wins';
+    }
+    if (oneChoice === 'spock' && twoChoice === "paper") {
+      result = 'Paper disproves Spock - paper wins';
+    }
+    if (oneChoice === 'spock' && twoChoice === "scissors") {
+      result = 'Spock smashes scissors - Spock wins';
+    }
+    if (oneChoice === 'spock' && twoChoice === "lizard") {
+      result = 'Lizard poisons Spock - lizard wins';
+    }
+
+    ///display result
     resultText.innerHTML = result;
-}
+
 }
